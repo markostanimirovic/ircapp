993295b65e7276cc808967daf55fe7575d9f1f45
edissarda17414 <edissarda@yahoo.com>
2017-03-08 19:50:10 +0100

    Promene na vagrantfile grani

--- IRCApp/src/util/FileIO.java
+++ IRCApp/src/util/FileIO.java
@@ -12,8 +12,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,38 +21,39 @@ import java.util.logging.Logger;
  */
 public class FileIO {
 
+    // nova grana
+    
     public static void vagrantConfig(String putanja) {
 
         File file = new File(putanja + "\\Vagrantfile");
-        
-       System.out.println(file.toString());
-        
-        while (!file.exists())
-                ;
-        
+
+        System.out.println(file.toString());
+
+        while (!file.exists());
+
         if (file.exists()) {
             try {
                 BufferedReader IN = new BufferedReader(new FileReader(file));
                 String rezultatIzFajla = "";
                 String pom;
-                while ((pom = IN.readLine()) != null)
+                while ((pom = IN.readLine()) != null) {
                     rezultatIzFajla = rezultatIzFajla + pom + System.lineSeparator();
-                
+                }
+
                 System.out.println(rezultatIzFajla);
                 IN.close();
                 String upisUFajl = "";
-                
+
                 upisUFajl = rezultatIzFajla.substring(0, rezultatIzFajla.length() - 5);
                 upisUFajl = upisUFajl + "  config.vm.provision \"shell\", path: \"script.sh\"\nend\n";
                 System.out.println(upisUFajl);
-                
+
                 BufferedWriter out = new BufferedWriter(new FileWriter(file));
-                
+
                 out.flush();
                 out.write(upisUFajl);
-                
+
                 out.close();
-                
 
             } catch (FileNotFoundException ex) {
                 Logger.getLogger(FileIO.class.getName()).log(Level.SEVERE, null, ex);
@@ -63,5 +62,5 @@ public class FileIO {
             }
         }
     }
-    
+
 }

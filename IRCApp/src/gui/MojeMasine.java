1e3cc4d46920b9289a885a1487efb88e6b2b0dae
aleksandarcolic22414 <acacolic@gmail.com>
2017-03-23 17:58:07 +0100

    Vracena maretova funkcija. Usporena aplikacija opet.

--- IRCApp/src/gui/MojeMasine.java
+++ IRCApp/src/gui/MojeMasine.java
@@ -52,6 +52,7 @@ public class MojeMasine extends javax.swing.JFrame {
         jbtnObrisi = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jtxtPutanja = new javax.swing.JTextField();
+        jbtnOsvezi = new javax.swing.JButton();
 
         jmnRefresh.setText("Refresh");
         jmnRefresh.addActionListener(new java.awt.event.ActionListener() {
@@ -115,6 +116,16 @@ public class MojeMasine extends javax.swing.JFrame {
         jtxtPutanja.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jtxtPutanja.setDisabledTextColor(new java.awt.Color(0, 0, 0));
 
+        jbtnOsvezi.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jbtnOsvezi.setText("Osveži");
+        jbtnOsvezi.setMaximumSize(new java.awt.Dimension(84, 29));
+        jbtnOsvezi.setPreferredSize(new java.awt.Dimension(84, 29));
+        jbtnOsvezi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtnOsveziActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -123,15 +134,17 @@ public class MojeMasine extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 354, Short.MAX_VALUE)
+                        .addComponent(jScrollPane1)
                         .addGap(10, 10, 10))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jbtnPokreni, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(51, 51, 51)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jbtnZaustavi, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(30, 30, 30)
                         .addComponent(jbtnObrisi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap())
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jbtnOsvezi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addGap(0, 0, Short.MAX_VALUE))
@@ -152,7 +165,8 @@ public class MojeMasine extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jbtnPokreni, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jbtnZaustavi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jbtnObrisi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jbtnObrisi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jbtnOsvezi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -205,9 +219,13 @@ public class MojeMasine extends javax.swing.JFrame {
     }//GEN-LAST:event_jlMojeMasineMouseClicked
 
     private void jmnRefreshActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmnRefreshActionPerformed
+        
+    }//GEN-LAST:event_jmnRefreshActionPerformed
+
+    private void jbtnOsveziActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnOsveziActionPerformed
         dlm.clear();
         Kontroler.ucitajMojeMasine();
-    }//GEN-LAST:event_jmnRefreshActionPerformed
+    }//GEN-LAST:event_jbtnOsveziActionPerformed
 
     /**
      * @param args the command line arguments
@@ -219,6 +237,7 @@ public class MojeMasine extends javax.swing.JFrame {
     private javax.swing.JPopupMenu jPopupMenu1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton jbtnObrisi;
+    private javax.swing.JButton jbtnOsvezi;
     private javax.swing.JButton jbtnPokreni;
     private javax.swing.JButton jbtnZaustavi;
     private javax.swing.JList<String> jlMojeMasine;
